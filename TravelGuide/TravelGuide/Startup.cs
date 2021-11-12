1c1ae7ac4ae4edc30b09ffa4e2173afd7b2a8564
prcstak <g.marsel2002@gmail.com>
2021-11-12 21:37:56 +0300

    implement MVC & use OpS html template

--- TravelGuide/TravelGuide/Startup.cs
+++ TravelGuide/TravelGuide/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -33,12 +34,7 @@ namespace TravelGuide
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -47,7 +43,8 @@ namespace TravelGuide
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
+            app.UseEndpoints(endpoints => 
+                endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello")));
         }
     }
 }
