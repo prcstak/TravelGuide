98c06fe7c1bca37dab08cd6ead1c8640674946f1
prcstak <g.marsel2002@gmail.com>
2021-11-17 12:21:38 +0300

    Complete Layout MVC не работает, нарушено распознавание пути к файлу

--- TravelGuide/TravelGuide/Startup.cs
+++ TravelGuide/TravelGuide/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -35,16 +36,18 @@ namespace TravelGuide
                 app.UseDeveloperExceptionPage();
             }
             
-
             app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => 
-                endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello")));
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
         }
     }
 }
