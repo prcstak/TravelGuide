f1648b0c7792e62a1efccd69f58cc0d10eaa6b13
prcstak <g.marsel2002@gmail.com>
2021-11-17 13:06:14 +0300

    add registration & authorization pages

---
+++ TravelGuide/TravelGuide/Pages/Account/Authorization.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelGuide.Pages.Account
+{
+    public class AuthorizationModel : PageModel
+    {
+        [BindProperty]
+        public Profile Profile { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            
+        }
+
+    }
+
+    public class Profile
+    {
+        [EmailAddress(ErrorMessage = "Некорректный Email-адрес")]
+        [Required(ErrorMessage = "Необходимо ввести Email")]
+        public string Email { get; set; }
+        
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$", ErrorMessage = "Неверный пароль")]
+        [Required(ErrorMessage = "Необходимо ввести пароль")]
+        public string Password { get; set; }
+    }
+}
---
+++ TravelGuide/TravelGuide/Pages/Account/RegistrationUser.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TravelGuide.Pages.Account
+{
+    public class RegistrationUser : PageModel
+    {
+        [BindProperty]
+        public GetInfo GetInfo { get; set; }
+        
+        public void OnGet()
+        {
+            
+        }
+    }
+
+    public class GetInfo
+    {
+        [EmailAddress(ErrorMessage = "Некорректный Email-адрес")]
+        [Required(ErrorMessage = "Необходимо ввести Email")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Полезная информация для аккаунта")]
+        public string PhoneNumber { get; set; }
+        
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$", ErrorMessage = "Неверный пароль")]
+        [Required(ErrorMessage = "Необходимо придумать пароль")]
+        public string Password { get; set; }
+        
+        [Compare(nameof(Password), ErrorMessage = "Пароли не совпадают")]
+        [Required(ErrorMessage = "Необходимо повторить ввод пароль")]
+        public string PasswordConfirm { get; set; }
+    }
+}
