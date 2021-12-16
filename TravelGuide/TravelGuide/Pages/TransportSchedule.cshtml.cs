1b7a0c16c060f8576368539c389aa25b988364f1
prcstak <g.marsel2002@gmail.com>
2021-12-16 12:41:58 +0300

    add view for schedule table

--- TravelGuide/TravelGuide/Pages/TransportSchedule.cshtml.cs
+++ TravelGuide/TravelGuide/Pages/TransportSchedule.cshtml.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TravelGuide.Pages
 {
     public class TransportSchedule : PageModel
     {
+        [BindProperty]
+        public List<Schedule> Schedule { get; set; } 
+        public Schedule LSchedule { get; set; }
         public void OnGet()
         {
-            
+            Schedule = new List<Schedule>(){new Schedule("15:10", "fg","ffg")} ;
+        }
+    }
+
+    public class Schedule
+    {
+        public string Time { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+
+        public Schedule(string time, string from, string to)
+        {
+            Time = time;
+            From = from;
+            To = to;
         }
     }
 }
