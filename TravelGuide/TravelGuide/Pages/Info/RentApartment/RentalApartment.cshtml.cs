3820d057f824a7b74151d721ad75d47eb976dcbf
prcstak <g.marsel2002@gmail.com>
2022-01-11 19:04:29 +0300

    Add filter

--- TravelGuide/TravelGuide/Pages/Info/RentApartment/RentalApartment.cshtml.cs
+++ TravelGuide/TravelGuide/Pages/Info/RentApartment/RentalApartment.cshtml.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
 using System.Threading.Tasks;
 using DataBase.Migrations;
 using DataBase.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace TravelGuide.Pages
@@ -14,6 +18,14 @@ namespace TravelGuide.Pages
 
         public IWebHostEnvironment _appEnvironment;
 
+        [BindProperty]
+        public Filter Filter { get; set; } = new Filter()
+        {
+            Rooms = new SelectList(new List<int> {1, 2, 3, 4, 5}),
+            Duration = new SelectList(new List<string> {"сутки", "неделя", "месяц"})
+        };
+
+        public string Result { get; set; }
         public RentalApartment(DataBase.Access.Context _db, IWebHostEnvironment appEnvironment)
         {
             db = _db;
@@ -22,9 +34,36 @@ namespace TravelGuide.Pages
         
         public List<Аdvertisement> Advertisements { get; set; }
 
-        public async Task OnGet()
+        public async Task OnGet(string? dur, int? room)
         {
-            Advertisements = await db.Advertisement.Include(p => p.Person).ToListAsync();
+            Advertisements = await db.Advertisement
+                .Include(p => p.Person)
+                .ToListAsync();
+            var roomResult = "любое";
+            if (dur != null)
+            {
+                Advertisements = Advertisements
+                    .Where(w => w.Duration == dur).ToList();
+            }
+            else
+            {
+                dur = "любой";
+            }
+            if (room != null)
+            {
+                Advertisements = Advertisements
+                    .Where(w => w.Rooms == room).ToList();
+                roomResult = room.ToString();
+            }
+            ViewData.Add("dur", dur);
+            ViewData.Add("room", roomResult);
+            Result = $"Все объявления по запросу: Срок сдачи: {dur}" + "\n" + $"Количество комнат: {roomResult}";
         }
+    }
+
+    public class Filter
+    {
+        public SelectList Rooms { get; set; }
+        public SelectList Duration { get; set; }
     }
 }
